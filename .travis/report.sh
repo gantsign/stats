cc0d51d28c79966fe2edb5ccf64733e8d8d5fb28
John Freeman <john.freeman.opensource@gmail.com>
2018-09-04 09:35:36 +0000

    Added Travis CI config
    To generate reports.

---
+++ .travis/report.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+repo_url=https://${GITHUB_TOKEN}@github.com/gantsign/stats.git
+
+git clone --single-branch --branch=gh-pages --depth=1 "$repo_url" tmp
+
+push() {
+    git config user.email 'gantsignbot@gmail.com'
+    git config user.name 'GantSign [bot]'
+
+    git add --verbose .
+
+    git commit --message="\
+Updated project data
+
+Using report script.
+"
+
+    git push "$repo_url" gh-pages
+}
+
+pwd
+
+pipenv run python report.py tmp/data
+
+(cd tmp && push)
